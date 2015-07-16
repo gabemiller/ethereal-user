f482e75fd6af1304ed560e1c6ff677c40fbb07dc
Gabor Molnar <gabormolnar92@gmail.com>
2015-07-16 21:19:23 +0200

    Fix routes name

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -11,10 +11,24 @@ Route::group(['namespace' => 'Ethereal\User\Controllers'], function () {
     Route::group(['prefix' => 'admin'], function () {
 
         // Resource route for UserController
-        Route::resource(trans('ethereal-user::routes.user.slug'), 'UserController', ['middleware' => 'auth']);
+        Route::resource(trans('ethereal-user::routes.user.slug'), 'UserController',
+            ['middleware' => 'auth',
+                'names' => ['index' => 'admin.user.index',
+                    'create' => 'admin.user.create',
+                    'store' => 'admin.user.store',
+                    'edit' => 'admin.user.edit',
+                    'update' => 'admin.user.update',
+                    'destroy' => 'admin.user.destroy']]);
 
         // Resource route for RoleController
-        Route::resource(trans('ethereal-user::routes.role.slug'), 'RoleController', ['middleware' => 'auth']);
+        Route::resource(trans('ethereal-user::routes.role.slug'), 'RoleController',
+            ['middleware' => 'auth',
+                'names' => ['index' => 'admin.role.index',
+                    'create' => 'admin.role.create',
+                    'store' => 'admin.role.store',
+                    'edit' => 'admin.role.edit',
+                    'update' => 'admin.role.update',
+                    'destroy' => 'admin.role.destroy']]);
     });
 
 
