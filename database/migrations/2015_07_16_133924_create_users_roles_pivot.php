cb4cfdee032a2c910afbef69b8a64a92ffe76826
Gabor Molnar <gabormolnar92@gmail.com>
2015-07-21 18:58:22 +0200

    Fix migrations

--- database/migrations/2015_07_16_133924_create_users_roles_pivot.php
+++ database/migrations/2015_07_16_133924_create_users_roles_pivot.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRolesUsersPivot extends Migration
+class CreateUsersRolesPivot extends Migration
 {
     /**
      * Run the migrations.
